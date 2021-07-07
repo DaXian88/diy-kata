dc78448be55fe728e21d62380988cb835c9b038a
Khalid Safy <khalid_safy88@hotmail.com>
2021-07-07 16:11:50 +0100

    Completed fizzBuzz function/tests

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+	if(Number.isInteger(number / 3) && Number.isInteger(number / 5)) {
+	return "FizzBuzz"
+}
+
+	else if(Number.isInteger(number / 3)) {
+	return "Fizz"
+}
+	else if(Number.isInteger(number / 5)) {
+	return "Buzz"
+}
+	else{
+	return number
+}
+};
 
 module.exports = fizzBuzz;
